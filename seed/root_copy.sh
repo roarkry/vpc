587e9c3356d42e384fe620b273e3e4e3787fdfa2
roarkry <roarkry@gmail.com>
2017-03-25 22:06:59 -0500

    update copy script to ensure rclone doesn't run in parallel

--- seed/root_copy.sh
+++ seed/root_copy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "BEGIN COPY @ $(date '+%Y/%m/%d_%H:%M:%S')"
 if mount | grep encfs > /dev/null; then
-
+if pidof -o %PPID -x "rclone"; then
+echo "COPY ALREADY IN PROGRESS"
+else
 while : ; do
    start=$SECONDS
-   su -c "/home/plex/vpc/seed/copy.sh" -s /bin/sh plex 
+   su -c "/home/plex/vpc/seed/copy.sh" -s /bin/sh plex
    finish=$SECONDS
    duration=$((finish - start))
    echo "Duration of Copy was $duration seconds"
@@ -12,6 +14,7 @@ while : ; do
 done
 rm -Rf /home/plex/completed/*
 echo "COPY COMPLETED"
+fi
 else
 echo "ENCFS NOT MOUNTED"
 fi
